91c6e7ede1c9113d5676fdcaa3a8f7e1ca1bcfd6
Thomas Hladki <tomhladki@icloud.com>
2024-03-26 02:12:55 -0400

    stock view work

--- app/src/main/java/com/comp4200/project/papertrader/IndividualStockActivity.kt
+++ app/src/main/java/com/comp4200/project/papertrader/IndividualStockActivity.kt
@@ -4,48 +4,50 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-//import com.squareup.picasso.Picasso
-//import retrofit2.Call
-//import retrofit2.Callback
-//import retrofit2.Response
+import com.comp4200.project.papertrader.models.StockDto
+import com.comp4200.project.papertrader.services.StockService
+import com.squareup.picasso.Picasso
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class IndividualStockActivity : AppCompatActivity() {
 
+    private var stockService = StockService(OkHttpClient())
     private lateinit var token: String
+    private lateinit var ticker: String
+    private lateinit var dto: StockDto
     private var quantityOwned: Int = 0 // Initialize with default value
     private var stockPrice: Double = 0.0 // Initialize with default value
+
+    private lateinit var quantityTextView: TextView
+    private lateinit var priceTextView: TextView
+    private lateinit var tickerTextView: TextView
+    private lateinit var totalValTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.individual_stock_activity)
 
-        val stockTicker = intent.getStringExtra("STOCK_TICKER") ?: ""   //Needs to be passed when starting activity
+        this.quantityTextView = findViewById<TextView>(R.id.quantity)
+        this.priceTextView = findViewById<TextView>(R.id.price)
+        this.tickerTextView = findViewById<TextView>(R.id.stockTicker)
+        this.totalValTextView = findViewById<TextView>(R.id.totalValue)
+
+        this.ticker = intent.getStringExtra("STOCK_TICKER") ?: ""   //Needs to be passed when starting activity
+
+        //initialize dto
+        this.dto = StockDto("", "", "") //finish later
+
+        var stockData = this.stockService.getStockData(dto)
 
 
-//        val retrofit = Retrofit.Builder()
-//            .baseUrl("https://api.example.com/") // Replace with actual API base URL
-//            .addConverterFactory(GsonConverterFactory.create())
-//            .build()
-//
-//        val service = retrofit.create(StockService::class.java)
-//        val call = service.getStockData(token, symbol) // Pass token to API call
-//
-//        call.enqueue(object : Callback<StockData> {
-//            override fun onResponse(call: Call<StockData>, response: Response<StockData>) {
-//                if (response.isSuccessful) {
-//                    val stockData = response.body()
-//
-//                    // Update UI with stock data
-//                    updateUI(stockData)
-//                } else {
-//                    // Handle unsuccessful response
-//                }
-//            }
-//
-//            override fun onFailure(call: Call<StockData>, t: Throwable) {
-//                // Handle failure
-//            }
-//        })
+
+
+
     }
 
 
