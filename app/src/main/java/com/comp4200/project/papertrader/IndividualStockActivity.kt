18402a9ea1630115f0e61059550c74d27f8a72df
Thomas Hladki <tomhladki@icloud.com>
2024-03-27 00:08:10 -0400

    Implement full basic functionality

--- app/src/main/java/com/comp4200/project/papertrader/IndividualStockActivity.kt
+++ app/src/main/java/com/comp4200/project/papertrader/IndividualStockActivity.kt
@@ -4,22 +4,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.comp4200.project.papertrader.models.BuySellDto
 import com.comp4200.project.papertrader.models.StockDto
 import com.comp4200.project.papertrader.models.UserStockModel
 import com.comp4200.project.papertrader.services.StockService
 import com.comp4200.project.papertrader.services.TokenService
-//import com.squareup.picasso.Picasso
 import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+
 
 class IndividualStockActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,15 +38,20 @@ class IndividualStockActivity : AppCompatActivity() {
                 var backBtn = findViewById<Button>(R.id.backButton)
                 var buyBtn = findViewById<Button>(R.id.buyButton)
                 var sellBtn = findViewById<Button>(R.id.sellButton)
+                var sellQuantity = findViewById<EditText>(R.id.sellQuantity)
+                var buyQuantity = findViewById<EditText>(R.id.buyQuantity)
 
                 var stockData = stockService.getStockData(dto)
 
                 var userStockModel = findStockInList(stockService.getUserStockList(token), ticker)
 
-                quantityTextView.setText("Quantity owned: " + userStockModel.quantity)
+                var quantityOwned = userStockModel?.quantity ?: 0
+
+
+                quantityTextView.setText("Quantity owned: " + quantityOwned)
                 priceTextView.setText("Price per share: $" + stockData.close)
                 tickerTextView.setText(ticker)
-                totalValTextView.setText("Value owned: $" + userStockModel.quantity * userStockModel.price)
+                totalValTextView.setText("Value owned: $" + quantityOwned * stockData.close)
 
 
                 backBtn.setOnClickListener{
@@ -58,12 +60,19 @@ class IndividualStockActivity : AppCompatActivity() {
                     finish()
                 }
 
-                buyBtn.setOnClickListener{
 
+                buyBtn.setOnClickListener{
+                    lifecycleScope.launch {
+                        var buyDto = BuySellDto(ticker, buyQuantity.text.toString().toInt())
+                        stockService.BuyStock(token, buyDto)
+                    }
                 }
 
                 sellBtn.setOnClickListener{
-
+                    lifecycleScope.launch {
+                        var sellDto = BuySellDto(ticker, sellQuantity.text.toString().toInt())
+                        stockService.SellStock(token, sellDto)
+                    }
                 }
 
             }catch(e: Exception){
