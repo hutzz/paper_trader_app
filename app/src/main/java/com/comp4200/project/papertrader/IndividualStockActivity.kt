ab1fe71cd284060f4b05c0f88d81e45f60a3e088
Zach <zachhutz0@gmail.com>
2024-03-28 13:43:04 -0400

    added retry mechanism

--- app/src/main/java/com/comp4200/project/papertrader/IndividualStockActivity.kt
+++ app/src/main/java/com/comp4200/project/papertrader/IndividualStockActivity.kt
@@ -1,14 +1,20 @@
 package com.comp4200.project.papertrader
 
+import android.content.Context
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.comp4200.project.papertrader.models.BuySellDto
 import com.comp4200.project.papertrader.models.StockDto
 import com.comp4200.project.papertrader.models.StockModel
@@ -16,9 +22,11 @@ import com.comp4200.project.papertrader.models.UserStockModel
 import com.comp4200.project.papertrader.services.StockService
 import com.comp4200.project.papertrader.services.TokenService
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
+import java.io.IOException
 
 class IndividualStockActivity : AppCompatActivity() {
 
@@ -39,6 +47,8 @@ class IndividualStockActivity : AppCompatActivity() {
     private lateinit var stockData: StockModel
     private var quantityOwned = 0
 
+    private val MAX_RETRIES = 10
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.individual_stock_activity)
@@ -53,109 +63,110 @@ class IndividualStockActivity : AppCompatActivity() {
         sellQuantity = findViewById<EditText>(R.id.sellQuantity)
         buyQuantity = findViewById<EditText>(R.id.buyQuantity)
 
-        lifecycleScope.launch{
+        lifecycleScope.launch {
+            retryFetchUserDataAndStocks()
+        }
+    }
 
-            try{
-                ticker = intent.getStringExtra("STOCK_TICKER") ?: ""
-                this@IndividualStockActivity.token = tokenService.getAccessToken() ?: throw IllegalStateException("Access token is null")
-                this@IndividualStockActivity.dto = StockDto(ticker, "1d", "1d")
-                this@IndividualStockActivity.stockData = fetchStockData(dto)                                        //API CALL
-                var userStockModel = findStockInList(fetchUserStockList(token), ticker)                             //API CALL
-                this@IndividualStockActivity.quantityOwned = userStockModel?.quantity ?: 0
-                quantityTextView.setText("Quantity owned: " + this@IndividualStockActivity.quantityOwned)
-                priceTextView.setText("Price per share: $" + String.format("%.2f", stockData.close.first()))
-                tickerTextView.setText(ticker)
-                totalValTextView.setText("Value owned: $" + String.format("%.2f", quantityOwned * stockData.close.first()))
+    private suspend fun retryFetchUserDataAndStocks() {
+        var retryCount = 0
+        var success = false
+        while (!success && retryCount < MAX_RETRIES) {
+            try {
+                fetchDataAndStocks()
+                success = true
+            } catch (e: Exception) {
+                Log.e("IndividualStockActivity", "Failed to fetch data and stocks: $e")
+                delay(1000L)
+                retryCount++
+            }
+        }
+        if (!success) {
+            Log.e("IndividualStockActivity", "Failed to fetch data and stocks after $MAX_RETRIES attempts")
+        }
+    }
 
-                backBtn.setOnClickListener {
-                    lifecycleScope.launch {
-                        withContext(Dispatchers.IO) {
-                            Log.i("individual", "here")
-                            tokenService.checkTokenExpiration(tokenService.getAccessToken())
-                        }
-                        val intent = Intent(this@IndividualStockActivity, DashboardActivity::class.java)
-                        startActivity(intent)
-                        finish()
-                    }
+    private suspend fun fetchDataAndStocks() {
+        ticker = intent.getStringExtra("STOCK_TICKER") ?: ""
+        token = tokenService.getAccessToken() ?: throw IllegalStateException("Access token is null")
+        dto = StockDto(ticker, "1d", "1d")
+        stockData = fetchStockData(dto)
+        val userStockModel = findStockInList(fetchUserStockList(token), ticker)
+        quantityOwned = userStockModel?.quantity ?: 0
+        quantityTextView.text = "Quantity owned: $quantityOwned"
+        priceTextView.text = "Price per share: $${String.format("%.2f", stockData.close.first())}"
+        tickerTextView.text = ticker
+        totalValTextView.text = "Value owned: $${String.format("%.2f", (quantityOwned * stockData.close.first()))}"
+        setupButtons()
+    }
+
+    private fun setupButtons() {
+        backBtn.setOnClickListener {
+            lifecycleScope.launch {
+                withContext(Dispatchers.IO) {
+                    tokenService.checkTokenExpiration(tokenService.getAccessToken())
                 }
+                val intent = Intent(this@IndividualStockActivity, DashboardActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
 
-                buyBtn.setOnClickListener{
-                    lifecycleScope.launch {
-                        val quant = buyQuantity.text.toString().toInt()
-                        val buyDto = BuySellDto(ticker, quant)
-                        val resp = buyStock(token, buyDto)
-                        if(resp.contains("Successfully purchased")){
-                            updateUI(quant, stockData.close.first())
-                        }
-
-                        Toast.makeText(this@IndividualStockActivity, resp, Toast.LENGTH_LONG).show()
-                    }
+        buyBtn.setOnClickListener {
+            lifecycleScope.launch {
+                val quant = buyQuantity.text.toString().toInt()
+                val buyDto = BuySellDto(ticker, quant)
+                val resp = buyStock(token, buyDto)
+                if (resp.contains("Successfully purchased")) {
+                    updateUI(quant, stockData.close.first())
                 }
+                Toast.makeText(this@IndividualStockActivity, resp, Toast.LENGTH_LONG).show()
+            }
+        }
 
-                sellBtn.setOnClickListener{
-                    lifecycleScope.launch {
-                        val quant = sellQuantity.text.toString().toInt()
-                        val sellDto = BuySellDto(ticker, quant)
-                        val resp = sellStock(token, sellDto)
-                        if(resp.contains("Successfully sold")){
-                            updateUI(quant, stockData.close.first())
-                        }
-
-                        Toast.makeText(this@IndividualStockActivity, resp, Toast.LENGTH_LONG).show()
-                    }
+        sellBtn.setOnClickListener {
+            lifecycleScope.launch {
+                val quant = sellQuantity.text.toString().toInt()
+                val sellDto = BuySellDto(ticker, quant)
+                val resp = sellStock(token, sellDto)
+                if (resp.contains("Successfully sold")) {
+                    updateUI(-quant, stockData.close.first())
                 }
-
-            }catch(e: Exception){
-                Log.e("DashboardError", "Failed to get user details: ", e)
+                Toast.makeText(this@IndividualStockActivity, resp, Toast.LENGTH_LONG).show()
             }
         }
     }
 
-    private fun findStockInList(list: List<UserStockModel>, ticker: String): UserStockModel?{
-        Log.i("TICKER VALUE:", ticker)
-        Log.i("STOCK LIST", list.toString())
-
-        var stock: UserStockModel? = null
-        list.forEach { userStockModel ->
-            if(userStockModel.symbol.lowercase() == ticker.lowercase()){
-                stock = userStockModel
-                return@forEach
-            }
-        }
-
-        return stock
+    private fun findStockInList(list: List<UserStockModel>, ticker: String): UserStockModel? {
+        return list.firstOrNull { it.symbol.lowercase() == ticker.lowercase() }
     }
 
-
-    private fun updateUI(quantity: Int, price: Float){
-        var quantityTextView = findViewById<TextView>(R.id.quantity)
-        var totalValTextView = findViewById<TextView>(R.id.totalValue)
-        this@IndividualStockActivity.quantityOwned += quantity
-
-        quantityTextView.setText("Quantity owned: " + this@IndividualStockActivity.quantityOwned)
-        totalValTextView.setText("Value owned: $" + this@IndividualStockActivity.quantityOwned * price)
+    private fun updateUI(quantity: Int, price: Float) {
+        quantityOwned += quantity
+        quantityTextView.text = "Quantity owned: $quantityOwned"
+        totalValTextView.text = "Value owned: $${quantityOwned * price}"
     }
 
-    private suspend fun fetchStockData(dto: StockDto): StockModel{
+    private suspend fun fetchStockData(dto: StockDto): StockModel {
         return withContext(Dispatchers.IO) {
             stockService.getStockData(dto)
         }
     }
 
-    private suspend fun fetchUserStockList(token: String): List<UserStockModel>{
-        return withContext(Dispatchers.IO){
+    private suspend fun fetchUserStockList(token: String): List<UserStockModel> {
+        return withContext(Dispatchers.IO) {
             stockService.getUserStockList(token)
         }
     }
 
-    private suspend fun buyStock(token: String, buyDto: BuySellDto): String{
-        return withContext(Dispatchers.IO){
+    private suspend fun buyStock(token: String, buyDto: BuySellDto): String {
+        return withContext(Dispatchers.IO) {
             stockService.BuyStock(token, buyDto).msg
         }
     }
 
-    private suspend fun sellStock(token: String, sellDto: BuySellDto): String{
-        return withContext(Dispatchers.IO){
+    private suspend fun sellStock(token: String, sellDto: BuySellDto): String {
+        return withContext(Dispatchers.IO) {
             stockService.SellStock(token, sellDto).msg
         }
     }
